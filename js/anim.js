58df61df6d6afab0efd9e60eca2ba4d360676845
Trencsánszky Patrik <90695000+TrencsanszkyPatrik@users.noreply.github.com>
2023-12-13 21:38:19 +0100

    Add files via upload

--- js/anim.js
+++ js/anim.js
@@ -6,3 +6,18 @@ function showFooterNav() {
 function megnyit(link){
     window.open(link, "_self")
 }
+
+
+let cards = document.querySelectorAll('.card');
+let iframes = document.querySelectorAll('.iframe');
+
+cards.forEach((item, index) =>{
+    item.addEventListener('mouseover', function(){
+        let vSrc = iframes[index].dataset.video;
+        iframes[index].src = vSrc;
+    });
+    item.addEventListener('mouseleave', function (){
+        iframes[index].src = '';
+    });
+
+})
